3876596973fcc5652d2c4bdbc3b097613b5b3537
neolkk <47995358+Neolkk@users.noreply.github.com>
2025-01-09 11:11:26 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 import os
 print "test1"
 print "test2"
+
+https://registry.ollama.ai/v2/library/llama3.3/blobs/sha256:4824460d29f2058aaf6e1118a63a7a197a09bed509f0e7d4e2efb1ee273b447d
+https://registry.ollama.ai/v2/library/llama3.3/blobs/sha256:948af2743fc78a328dcb3b0f5a31b3d75f415840fdb699e8b1235978392ecf85
+https://registry.ollama.ai/v2/library/llama3.3/blobs/sha256:bc371a43ce90cc42fc9abb0d89a5959fbae91a53792d4dcd9b51aa48bd369b06
+https://registry.ollama.ai/v2/library/llama3.3/blobs/sha256:53a87df39647944ad2f0a3010a1d4a60ba76a1f8d5025bb7e76986e966d28ab6
+https://registry.ollama.ai/v2/library/llama3.3/blobs/sha256:56bb8bd477a519ffa694fc449c2413c6f0e1d3b1c88fa7e3c9d88d3ae49d4dcb
+
+%APPDATA%\ollama\models
