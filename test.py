ca1a5af05162e517b53ad6c9b16f80159d714de7
neolkk <47995358+Neolkk@users.noreply.github.com>
2025-01-10 10:59:03 +0900

    Update test.py

--- test.py
+++ test.py
@@ -8,4 +8,11 @@ https://registry.ollama.ai/v2/library/llama3.3/blobs/sha256:bc371a43ce90cc42fc9a
 https://registry.ollama.ai/v2/library/llama3.3/blobs/sha256:53a87df39647944ad2f0a3010a1d4a60ba76a1f8d5025bb7e76986e966d28ab6
 https://registry.ollama.ai/v2/library/llama3.3/blobs/sha256:56bb8bd477a519ffa694fc449c2413c6f0e1d3b1c88fa7e3c9d88d3ae49d4dcb
 
+https://registry.ollama.ai/v2/library/llama3.3/blobs/sha256-c7091aa45e9be6c15e1e5c8d5489d47f18183bf5077b3d3697924e1d18ad1b2a
 %APPDATA%\ollama\models
+
+
+C:\Users\leeki\.ollama\models\manifests\registry.ollama.ai\library\llama3.3\latest
+{"schemaVersion":2,"mediaType":"application/vnd.docker.distribution.manifest.v2+json","config":{"mediaType":"application/vnd.docker.container.image.v1+json","digest":"sha256:c7091aa45e9be6c15e1e5c8d5489d47f18183bf5077b3d3697924e1d18ad1b2a","size":562},"layers":[{"mediaType":"application/vnd.ollama.image.model","digest":"sha256:4824460d29f2058aaf6e1118a63a7a197a09bed509f0e7d4e2efb1ee273b447d","size":42520398528},{"mediaType":"application/vnd.ollama.image.template","digest":"sha256:948af2743fc78a328dcb3b0f5a31b3d75f415840fdb699e8b1235978392ecf85","size":1481},{"mediaType":"application/vnd.ollama.image.license","digest":"sha256:bc371a43ce90cc42fc9abb0d89a5959fbae91a53792d4dcd9b51aa48bd369b06","size":7627},{"mediaType":"application/vnd.ollama.image.license","digest":"sha256:53a87df39647944ad2f0a3010a1d4a60ba76a1f8d5025bb7e76986e966d28ab6","size":5622},{"mediaType":"application/vnd.ollama.image.params","digest":"sha256:56bb8bd477a519ffa694fc449c2413c6f0e1d3b1c88fa7e3c9d88d3ae49d4dcb","size":96}]}
+
+  
